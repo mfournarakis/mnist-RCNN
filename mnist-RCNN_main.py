083c9aa88eb82cca4ad7954f56ce3452118ce880
Marios <mfournarakis@gmail.com>
2018-07-27 16:39:29 +0100

    change forbenious loss to be mapped onto poalr coordinates

--- mnist-RCNN_main.py
+++ mnist-RCNN_main.py
@@ -183,7 +183,11 @@ def define_loss(args, x,y):
 
     if args.loss=='frobenius':
         forb_distance=torch.nn.PairwiseDistance()
-        loss=(forb_distance(x.view(-1,2),y.view(-1,2))**2).sum()
+        x_polar=x.view(-1,2)
+        x_polar=x/x.norm(p=2,dim=1)
+        y_polar=y.view(-1,2)
+        y_polar=y/y.norm(p=2,dim=1)
+        loss=(forb_distance(x_polar,y_polar)**2).sum()
 
     elif args.loss=='cosine_squared':
 
